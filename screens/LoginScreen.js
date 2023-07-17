1d1187cf326119ab96d33efedd3d09fdc9b3b7f4
alegarn <alexgarnier78310@protonmail.com>
2023-07-17 11:08:02 +0200

    allow authenticated when logged in

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -10,35 +10,25 @@ import { AuthContext } from '../store/auth-context';
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  const authContext = useContext(AuthContext);
-
   async function signInHandler({email, password}) {
     setIsAuthenticating(true);
     try {
-
-/*       const token = authContext.token;
-      const expiry = authContext.expiry;
-      const access_token = authContext.access_token; */
-      console.log("login", token, expiry, access_token);
-
       const response = await login({email, password});
       console.log("response login screen", response);
       setIsAuthenticating(false);
       if (response.status === 200) {
-        // Navigate to the "LoginScreen"
+        // Navigate to the "LoginScreen" set in App.js
         console.log('User login successful');
       } else {
-          // Handle other response statuses if needed
-
           console.log(response);
-
           Alert.alert('Invalid input', `${response}`);
-        }
+          setIsAuthenticating(false);
+        };
     } catch (err) {
       console.log(err);
       Alert.alert('There is an error', err);
       setIsAuthenticating(false);
-    }
+    };
   };
 
   if (isAuthenticating) {
